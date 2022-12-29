97022a1b9ef54278cc89168274f5baa21971c567
subhadra-parida <99946995+subhadra-parida@users.noreply.github.com>
2022-12-29 11:11:45 +0530

    Button3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React,{useState} from "react";
 import './App.css';
+export default function App() {
+  const [list1, setList1] = useState(["Subhadra","Parida","Odisha","Sarjapura"])
+  const [list2,setList2] = useState(["Manory","Jimik","Manipur","Bangalore"])
+  const [list3,setList3] = useState(["Muskan","Valmiki", "Pune", "Pune"])
+  const [maindata,setMaindata] = useState([])
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div id="container">
+      <button onClick={()=>{
+        setMaindata(list1)
+      }}>BUTTON1</button>
+     
+    <button onClick={()=>{
+        setMaindata(list2)
+      }}>BUTTON2</button>
+      
+
+    <button onClick={()=>{
+        setMaindata(list3)
+      }}>BUTTON3</button>
+      {
+        maindata.map((Event)=>{
+          return(
+            <h1 id="output">{Event}</h1>
+          )
+        })
+      }
     </div>
-  );
-}
+  )
 
-export default App;
+}
